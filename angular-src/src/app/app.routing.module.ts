ea0e6b6987a935472ac708e2bc2d8833e4b7f53e
Bernardo Revelles <brevelles@gmail.com>
2017-12-14 13:21:18 +0000

    angular setup

---
+++ angular-src/src/app/app.routing.module.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [],
+    imports: [],
+    providers: [],
+    bootstrap: []
+})
+
+export class AppRoutingModule { }
